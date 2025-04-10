b7dd72bd4eaab62fb6845593b0b3f3533e72ad7b
Aditya Dwi Rahmadi <adityadwi@monarchintiteknologi.com>
2025-04-10 11:35:07 +0700

    Add currency fields to currencies migration

--- database/migrations/2025_04_09_071748_create_currencies_table.php
+++ database/migrations/2025_04_09_071748_create_currencies_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('currencies', function (Blueprint $table) {
             $table->id();
+            $table->string('flag')->nullable();
+            $table->string('currency')->nullable();
+            $table->string('name')->nullable();
+            $table->string('denomination')->nullable();
+            $table->decimal('buy_rate', 15, 4)->nullable();
+            $table->decimal('sell_rate', 15, 4)->nullable();
+            $table->decimal('mid_rate', 15, 4)->nullable();
+            $table->decimal('max_addition', 15, 4)->nullable();
+            $table->decimal('max_reduction', 15, 4)->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
