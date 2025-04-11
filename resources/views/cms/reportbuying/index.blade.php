77966611dcb5b52533c5a3f3f5caf848198d953e
rezimonarchintiteknologi <reziferdiansyah@monarchintiteknologi.com>
2025-04-11 13:29:08 +0700

    update

--- resources/views/cms/reportbuying/index.blade.php
+++ resources/views/cms/reportbuying/index.blade.php
@@ -198,7 +198,7 @@
                     </div>
                     </div>
 
-                    <div class="row m-10">
+                    <div class="row">
                      <div class="col-md-4">
                       <div class="mb-3">
                       <input type="checkbox" class="form-check-input" id="po" checked>
